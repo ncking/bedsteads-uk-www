015d1c87744523436d7f12854065111712b35edd
Nigel King <nigel.c.king@gmail.com>
2024-06-08 17:21:48 +0100

    stash

--- server/lib/emails.ts
+++ server/lib/emails.ts
@@ -8,13 +8,14 @@ const host = env.stringRequired('EMAIL_HOST')
 const port = env.intRequired('EMAIL_HOST_PORT')
 const user = env.stringRequired('EMAIL_USER')
 const pass = env.stringRequired('EMAIL_USER_PASSWORD')
+const toEmail = env.stringRequired('EMAIL_SEND_ENQUIRY')
 
 export const sendEmails = async () => {
   const data = await findUnsent()
   const arr = await data.toArray()
   log.info(`${arr.length} emails to send`)
   if (__DEV__) {
-    return log.info('DEVELOPMENT: skipping email')
+    //return log.info('DEVELOPMENT: skipping email')
   }
   await Promise.all(
     arr.map(async (doc) => {
@@ -35,14 +36,24 @@ const sendEmail = async (props) => {
   const { email, id, enquiry, url, title } = props
   const domain = `https://www.bedsteads-uk.co.uk`
   const html = emailTemplate(props)
+  const rec = {
+    from: '"Website enquiry 🌐" <website@bedsteads-uk.co.uk>', // sender address
+    to: toEmail, // list of receivers  , enquiries@bedsteads-uk.co.uk
+    subject: 'Website enquiry',
+    text: enquiry,
+    replyTo: email,
+    html,
+  }
 
-  const transporter = nodemailer.createTransport({
-    host,
-    port,
-    auth: {
-      user,
-      pass,
-    },
-  })
-  await transporter.sendMail(rec)
+
+  console.log(rec)
+  // const transporter = nodemailer.createTransport({
+  //   host,
+  //   port,
+  //   auth: {
+  //     user,
+  //     pass,
+  //   },
+  // })
+  // await transporter.sendMail(rec)
 }
