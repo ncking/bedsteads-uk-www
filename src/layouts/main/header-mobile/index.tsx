38a8bcdb54e695ff45723f621dc20e557462fb8e
Nigel King <nigel.c.king@gmail.com>
2024-06-21 07:17:20 +0100

    fix: mobile header txt

--- src/layouts/main/header-mobile/index.tsx
+++ src/layouts/main/header-mobile/index.tsx
@@ -8,46 +8,52 @@ import * as styles from './style.scss'
 export * from '../menu'
 
 export const HeaderMobile = ({ route, filters }) => {
-
   const { meta = {} } = route
   const itemPage = filters?.id// @todo replace with <Helmet/> style
   const gridPage = !!meta?.category
   const enableStickyHeader = route?.id === 'home'
   const catUrl = getPathname().split('/').splice(0, 2).join('/')
   const categoryLabel = 'furniture' === filters.category
-  ? 'furniture'
-  : `${filters.size || filters.category} beds`
+    ? 'furniture'
+    : `${filters.size || filters.category} beds`
   /**
-   * 
+   *
    */
   let children
   if (itemPage) {
-    children = <>
-      <Link href={catUrl} className="logotype">
-        <Button
-          icon="arrow"
-          className={styles.backBtn}
-          ariaLabel="back to list"
-        />
-        {categoryLabel}
-      </Link>
-      <ResultCount />
-    </>
-
-  } else if (gridPage) {
-    children = <>
-      <Link href={catUrl} className={cx(styles.addPadding, 'logotype')}>
-        {categoryLabel}
-      </Link>
-      <BurgerButton />
-    </>
-  } else {
-    children = <>
-     <Link href="/" className={cx(styles.addPadding, 'logotype')}>
-        BEDSTEADS
-      </Link>
-      <BurgerButton />
-    </>
+    children = (
+      <>
+        <Link href={catUrl} className="logotype">
+          <Button
+            icon="arrow"
+            className={styles.backBtn}
+            ariaLabel="back to list"
+          />
+          {categoryLabel}
+        </Link>
+        <ResultCount />
+      </>
+    )
+  }
+  else if (gridPage) {
+    children = (
+      <>
+        <Link href={catUrl} className={cx(styles.addPadding, 'logotype')}>
+          {categoryLabel}
+        </Link>
+        <BurgerButton />
+      </>
+    )
+  }
+  else {
+    children = (
+      <>
+        <Link href="/" className={cx(styles.addPadding, 'logotype')}>
+          BEDSTEADS
+        </Link>
+        <BurgerButton />
+      </>
+    )
   }
 
   setUnsetBodyClass(styles.fullHeight, enableStickyHeader)
