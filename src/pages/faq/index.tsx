a4e3f60a8d0d11374585a66054f097df5afedab2
Nigel King <nigel.c.king@gmail.com>
2024-06-24 07:56:05 +0100

    stash

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -1,6 +1,6 @@
 import { cx } from '@raiz/browser'
 import { BedsteadsImage, Intro } from '@components'
-import { t1, t2, t3, t4 } from './text'
+
 
 
 export default () => {
@@ -24,7 +24,103 @@ export default () => {
 
       <div className="flex flex-text">
         <h1 className="flex__col--full">Additional information</h1>
-        <div className="flex__col">{t1}{t2}{t3}{t4}</div>
+        <div className="flex__col">
+
+
+    {/* <h2>Classifications</h2> */}
+    <h2>What makes an Item Antique?</h2>
+    <p>
+        An antique is an item that is 100 years old or more. They are often items which are cherished due to their aesthetic or historical qualities.
+
+    </p>
+    <h2>What is the difference between Antique, Vintage and Retro?</h2>
+    <p>
+        Antique items are 100 years sold or more, vintage items are generally considered to be between 20 and 100 years old, and retro items tend to be around 20 years old or less.
+        <br />
+        Generally, with each of these classifications, the items tend to be collectible or sought after and aesthetically reflect the styles of their times.
+    </p>
+
+
+
+
+    <h2>Classifications</h2>
+    <p>
+        Across our website you will notice us using words such as ‘Victorian’, Classic, Louis etc. below is a list of definitions to help demystify what we antique sellers mean:
+
+    </p>
+
+
+
+
+    <h4>What does ‘Victorian’ mean?</h4>
+    <p>
+        This generally refers to beds constructed within the Victorian era of 1820 – 1914, around Queen Victoria’s reign.
+        <br />
+        Styles during this period straddle the movements of the time, a delightful mixture of Aestheticism which encourages beauty for beauty’s
+        sake, and the industrial revolution which demanded practicality and functionality.
+    </p>
+
+
+
+    <h4> Art Nouveaux</h4>
+    <p>
+        Originating in Europe, Art Nouveau furniture showcases sinuous and sculptural forms,
+        characterized by curving lines, graceful arches, and sensuous ornamentation
+        {/* (Taken from Nickle Bed) */}
+    </p>
+
+
+
+
+
+
+    <h4>What is a ‘Louis’ item of Furniture?</h4>
+    <p>
+        Distinguishing between Louis XV, Louis XVI, and Louis XIV antique furniture requires an understanding
+        of the distinct characteristics and styles associated with each period. Here are some key features to look for:
+    </p>
+    <h4> Louis XIV (mid-17th century to early 18th century)</h4>
+    <p>
+        Emphasis on grandeur and opulence.
+        Heavy and robust furniture designs.
+        Elaborate ornamentation, including intricate carving and gilding.
+        Baroque style with bold and exaggerated curves.
+        Rich materials such as walnut, oak, and mahogany.
+        Symmetry and balance in overall form.
+    </p>
+
+    <h4>Louis XV (early to mid-18th century)</h4>
+    <p>
+        Transition towards a more delicate and sensual style.
+        Curvaceous forms, characterized by sinuous lines and flowing contours.
+        Ornamentation featuring rocaille (shell-like) motifs, floral designs, and asymmetry.
+    </p>
+    <p>
+        Lighter construction compared to Louis XIV furniture.
+        Materials like rosewood, satinwood, and tulipwood commonly used.
+        Cabriole legs with intricate scrollwork and often adorned with ormolu (gilded bronze) mounts.
+    </p>
+
+    <h4>Louis XVI (late 18th century)</h4>
+    <p>
+        Neoclassical influence, reflecting a return to classical Greek and Roman aesthetics.
+        <br />
+        Clean lines and geometric shapes.
+        Symmetry and proportion in design.
+        More restrained ornamentation, with motifs such as laurel wreaths, urns, and fluting.
+        Lighter colours and pastel hues used in upholstery and finishes.
+        <br />
+        Mahogany, walnut, and fruitwoods frequently utilized.
+        Straight, tapered legs, often with fluted details
+    </p>
+
+
+
+
+
+
+
+        </div>
         <div className="flex__col">
 
         <h2>Names of Makers</h2>
