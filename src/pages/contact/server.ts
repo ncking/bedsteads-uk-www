48c9867e95259f142da5db88b2b2d655f0b41086
Nigel King <nigel.c.king@gmail.com>
2024-06-30 12:22:54 +0100

    stash

--- src/pages/contact/server.ts
+++ src/pages/contact/server.ts
@@ -1,29 +1,32 @@
 import { organizationJsonLd, createAbsoluteUrl, createOgUrl } from '@server'
 import {
-    company,
-    email,
-    streetAddress,
-    postcode,
-    tel
+  company,
+  email,
+  streetAddress,
+  postcode,
+  tel,
+  openingHours,
+  facebookLink,
+  instagramLink,
 } from '@server/config'
 
 
-export default async ({ request, response }) => {
-    const title = `Contact ${company} | Location | Hours of business | Sales and Services, Contact us`
-    const description = `tel: ${tel} email: ${email}. Located just to the SW of Bristol we are easily reached via the M5 motorway. address: ${streetAddress} ${postcode}`
 
-    response
-        .setRobots('index,follow')
-        // .setData({ content })
-        .setJsonLd(organizationJsonLd)
-        .setTitle(title)
-        .setDescription(description)
-        .setOg({
-            type: 'article',
-            title,
-            description,
-            url: createAbsoluteUrl(request.pathname),
-            site_name: 'Bedsteads',
-            image: createOgUrl('contact'),
-        }) // min 200 x 200 px
+export default async ({ request, response }) => {
+  const title = `Contact ${company} | Location | Hours of business | Sales and Services, Contact us`
+  const description = `tel: ${tel} email: ${email}. Located just to the SW of Bristol we are easily reached via the M5 motorway. address: ${streetAddress} ${postcode}`
+
+  response
+    .setRobots('index,follow')
+    .setJsonLd(organizationJsonLd)
+    .setTitle(title)
+    .setDescription(description)
+    .setOg({
+      type: 'article',
+      title,
+      description,
+      url: createAbsoluteUrl(request.pathname),
+      site_name: 'Bedsteads',
+      image: createOgUrl('contact'),
+    }) // min 200 x 200 px
 }
