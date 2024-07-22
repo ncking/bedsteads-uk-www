ce550c63a9ecae69810f8019a9fd7e26ed972435
Nigel King <nigel.c.king@gmail.com>
2024-07-22 09:26:34 +0100

    stash

--- _scripts/favicons/build.ts
+++ _scripts/favicons/build.ts
@@ -3,7 +3,7 @@ import fs from 'node:fs'
 import pngQuantBinaryPath from 'pngquant-bin'
 import shelljs from 'shelljs'
 import favicons from 'favicons'
-import * as config from '#config'
+import * as config from '../../src/server/config'
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import packageInfo from '../../package.json'  with { type: "json" };
