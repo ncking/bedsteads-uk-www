2b0a81ed2d0c4c4ef817788979fe497faf9b4a63
Nigel King <nigel.c.king@gmail.com>
2024-07-22 14:36:47 +0100

    refactor: mongodb lib

--- src/server/dbase/connect.ts
+++ src/server/dbase/connect.ts
@@ -1,17 +1,14 @@
 import { env } from '@raiz/cli'
 import { log } from '@raiz/core'
-import mongodb from 'mongodb'
+import { MongoClient, Db, Collection } from 'mongodb'
 
-/**
- * database
- */
 const ENV_SERVER_DBASE_URL = env.stringRequired('SERVER_DBASE_URL')
 const ENV_DATABASE = env.stringRequired('DATABASE')
 const ENV_DBASE_TABLE = env.stringRequired('DATABASE_TABLE')
 
 let conn
 
-export const connect = async (collection = ENV_DBASE_TABLE) => {
+export const connect = async (collection = ENV_DBASE_TABLE): Promise<Collection> => {
   if (!conn) {
     const connectionConfig = {
       serverSelectionTimeoutMS: 2000, // this is for the initial connection
@@ -20,12 +17,13 @@ export const connect = async (collection = ENV_DBASE_TABLE) => {
       maxPoolSize: 5,
     }
     const connectionString = `mongodb://${ENV_SERVER_DBASE_URL}`
-    conn = await mongodb.MongoClient.connect(connectionString, connectionConfig).catch((err) => {
+    conn = await MongoClient.connect(connectionString, connectionConfig).catch((err) => {
       log.fatal(err)
     })
     if (!conn) {
       log.fatal('No database')
     }
   }
-  return conn?.db(ENV_DATABASE).collection(collection)
+  const db: Db = conn?.db(ENV_DATABASE)
+  return db.collection(collection)
 }
