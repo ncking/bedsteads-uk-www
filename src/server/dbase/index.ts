46e291d6346dbe5dc2a93fd849427bfc6ce7926e
Nigel King <nigel.c.king@gmail.com>
2024-08-01 08:23:20 +0100

    stash

--- src/server/dbase/index.ts
+++ src/server/dbase/index.ts
@@ -1,3 +1,3 @@
 import mongodb from 'mongodb'
-import {connect} from '@raiz/nuggins/dbase/src/mongo'
-export const con = await connect(mongodb, { collections: ['enquiry', 'stock'] })
+import { connect } from '@raiz/nuggins/dbase/src/mongo'
+export const con = await connect(mongodb)
