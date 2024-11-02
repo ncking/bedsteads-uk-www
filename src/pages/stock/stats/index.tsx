2a93734705bfe5ad4e80b01e8edbeff263a4d7e3
ncking <nigel.c.king@gmail.com>
2024-11-02 19:35:58 +0000

    stash

--- src/pages/stock/stats/index.tsx
+++ src/pages/stock/stats/index.tsx
@@ -1,30 +1,42 @@
+//@ts-nocheck
+import { SkeletonText } from '@raiz/react'
 import * as styles from './stats.scss'
 
 
-const skelteton =[
-  ['a', 'a'],
-  ['a', 'a'],
-  ['a', 'a'],
-  ['a', 'a'],
-  ['a', 'a'],
-  ['a', 'a'],
-  ['a', 'a'],
-  ['a', 'a'],
-  ['a', 'da'],
+const skelteton = [
+  [8, 12],
+  [12, 14], [16, 12], [8, 12], [18, 12], [8, 12], [8, 12], 
 ]
 
 export const Stats = ({ rows }) => {
+  let trs = []
 
-  
+  if (rows.length) {
+    trs = rows.map(([label, value], i) => {
+      return (
+        <tr key={i}>
+          <td>{label}</td>
+          <td>{value}</td>
+        </tr>)
+
+    })
+
+  } else {
+    trs = skelteton.map(([a, b], i) => {
+      return (
+        <tr key={`${i}-s`}>
+          <td><SkeletonText ch={a} /></td>
+          <td><SkeletonText ch={b} /></td>
+        </tr>)
+
+    })
+  }
+
   return (
     <div className={styles.stats}>
       <table>
         <tbody>
-          {(rows.length  ? rows :  skelteton)?.map(([label, value],i) => (
-            <tr key={i}>
-              <td>{label}</td>
-              <td>{value}</td>
-            </tr>))}
+          {trs}
         </tbody>
       </table>
     </div>
